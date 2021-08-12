672ff0e958b76cf7a2d2562fc9e7a0b8b30a09d2
Zhefeng Chen <chzf_zju@163.com>
2021-08-12 21:01:00 +0800

    copy ch01 to ch02

---
+++ ch02/mydebug.lua
@@ -0,0 +1,50 @@
+#!/usr/bin/env lua
+
+local debug = require "debug"
+
+-- record breakpoint status
+local status = {}
+status.bpnum = 0        -- current breakpoint number
+status.bpid = 0         -- current breakpoint id
+status.bptable = {}     -- table to save breakpoint infos
+
+
+-- hook
+local function linehook (event, line)
+    local info = debug.getinfo(2, "nfS")
+    for _, v in pairs(status.bptable) do
+        if v.func == info.func and v.line == line then
+            local prompt = string.format("(%s)%s %s:%d\n", 
+                info.namewhat, info.name, info.short_src, line)
+            io.write(prompt)
+            debug.debug()
+        end
+    end
+end
+
+-- set breakpoint
+local function setbreakpoint(func, line)
+    status.bpid = status.bpid + 1
+    status.bpnum = status.bpnum + 1
+    status.bptable[status.bpid] = {func = func, line = line}
+    if status.bpnum == 1 then           -- first breakpoint
+        debug.sethook(linehook, "l")	-- set hook
+    end
+    return status.bpid                  --> return breakpoint id
+end
+
+
+-- remove breakpoint
+local function removebreakpoint(id)
+    status.bptable[id] = nil
+    status.bpnum = status.bpnum - 1
+    if status.bpnum == 0 then
+        debug.sethook()                 -- remove hook
+    end
+end
+
+
+return {
+    setbreakpoint = setbreakpoint,
+    removebreakpoint = removebreakpoint,
+}

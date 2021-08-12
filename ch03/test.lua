a9dde7b5d2ef5ea9cd380e71c1e7ac8f036cbe49
Zhefeng Chen <chzf_zju@163.com>
2021-08-13 00:00:23 +0800

    copy from ch02 to ch03

---
+++ ch03/test.lua
@@ -0,0 +1,35 @@
+local mydebug = require "mydebug"
+local setbp = mydebug.setbreakpoint
+local rmbp = mydebug.removebreakpoint
+getv = mydebug.getvarvalue
+
+g = 1
+
+local u = 2
+local function foo (n)
+    local a = 3
+    a = a + 1
+    u = u + 1
+    g = g + 1
+end
+
+local function bar (n)
+    n = n + 1
+end
+
+local id1 = setbp(foo, 12)
+local id2 = setbp(bar, 17)
+
+foo(10)
+bar(10)
+
+rmbp(id2)
+
+foo(20)
+bar(20)
+
+rmbp(id1)
+
+foo(30)
+bar(30)
+

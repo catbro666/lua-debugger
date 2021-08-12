a590b5b257fbc7548e4d62789c826e3cb2fb6ea5
Zhefeng Chen <chzf_zju@163.com>
2021-08-12 23:06:40 +0800

    feature: general variable print function

--- ch02/mydebug.lua
+++ ch02/mydebug.lua
@@ -44,7 +44,58 @@ local function removebreakpoint(id)
 end
 
 
+-- get variable from local, upvalue or _ENV
+-- modified from "Programming in Lua Fourth edition"
+local function _getvarvalue (name, level, isenv)
+    local value
+    local found = false
+
+    -- + 1 to correct the level to include _getvarvalue itself,
+    -- default by 5: getvarvalue, debug mainchunk, debug.debug, hook and foo
+    level = (level or 5) + 1
+    -- try local vars
+    for i = 1, math.huge do
+        local n, v = debug.getlocal(level, i)
+        if not n then break end
+        if n == name then
+            value = v
+            found = true
+            -- we didn't break here in order to get the local var with max index
+        end
+    end
+    if found then return "local", value end
+
+    -- try upvalues
+    local func = debug.getinfo(level, "f").func
+    for i = 1, math.huge do
+        local n, v = debug.getupvalue(func, i)
+        if not n then break end
+        if n == name then return "upvalue", v end
+    end
+
+    if isenv then return "noenv" end	-- avoid dead loop
+
+    -- try to get from _ENV
+    local _, env = _getvarvalue("_ENV", level, true)
+    if env then
+        return "global", env[name]
+    else
+        return "noenv"
+    end
+end
+
+-- wrap _getvarvalue, to print value
+local function getvarvalue (name, level)
+    local where, value = _getvarvalue(name, level)
+    if value then
+        print(where, value)
+    else
+        print(name, "not found")
+    end
+end
+
 return {
     setbreakpoint = setbreakpoint,
     removebreakpoint = removebreakpoint,
+    getvarvalue = getvarvalue,
 }
